afb3b1f577843561e754480492a80f605dcb19ad
kbtlsrc <kbtl.src@hotmail.com>
2021-10-24 19:25:52 +0300

    Application of Vertigo Games videos

---
+++ Assets/TopDownShooter/Scripts/Camera/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+        [SerializeField] Transform camera;
+        [SerializeField] CameraSetting setting;
+
+        void Update()
+        {
+            RotationFollow();
+            MovementFollow();
+        }
+
+        private void RotationFollow()
+        {
+
+            camera.rotation = Quaternion.Lerp(camera.rotation, 
+                Quaternion.LookRotation(target.position - camera.position),
+                Time.deltaTime * setting.Speed);
+
+        }
+
+        private void MovementFollow()
+        {
+            camera.position = Vector3.Lerp(camera.position, target.position + setting.PositionOffset,
+                Time.deltaTime * setting.PositionLerp);
+        }
+    }
+
+}
---
+++ Assets/TopDownShooter/Scripts/Camera/CameraSetting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    [CreateAssetMenu (menuName = "TopDownShooter/Camera/Camera Settings")]
+   
+    public class CameraSetting : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] private float speed = 1;
+        public float Speed { get { return speed; } }
+
+        [Header("Position")]
+        [SerializeField] private Vector3 positionOffset;
+        public Vector3 PositionOffset { get { return positionOffset; } }
+
+        [SerializeField] private float positionLerp;
+        public float PositionLerp { get { return positionLerp; } }
+    }
+
+}
